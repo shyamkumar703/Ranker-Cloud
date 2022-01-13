1ee108e87a133bc48c24b5c25da1c60a0e40be4a
Shyam Kumar <shyamkumar@berkeley.edu>
2022-01-13 09:10:49 -0800

    attempt to package poll ID in notification

--- functions/index.js
+++ functions/index.js
@@ -4,14 +4,18 @@ admin.initializeApp();
 
 exports.newVote = functions.firestore.document("polls/{documentId}")
     .onUpdate((change, context) => {
-        const message = {
-            notification: {
-                title: "Someone voted on your poll!",
-                body: "You have a new vote on your poll.",
-            },
-        };
+      const message = {
+        notification: {
+          title: "Rankr",
+          body: "You have a new vote on your poll.",
+        },
+        data: {
+          pollId: context.params.documentId
+        }
+      };
 
-        const tokens = [change.after.data().deviceId];
-        functions.logger.log("Fetched Device ID", tokens[0]);
-        return admin.messaging().sendToDevice(tokens, message);
+      const tokens = [change.after.data().deviceId];
+      functions.logger.log("Fetched Device ID", tokens[0]);
+      functions.logger.log("New vote on poll with ID, context.params.documentId)
+      return admin.messaging().sendToDevice(tokens, message);
     });
